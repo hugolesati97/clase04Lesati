7e17dc4a0080dd4b66473962c976f7e7ab256e22
hugolesati <hugolesati@hotmail.com>
2021-08-04 22:03:31 -0300

    Actualizacion

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
-/* function saludar() {
-    console.log("Hola Chicos");
+/* function suma(a, b) {
+    let r = a + b
+    alert(r);
 }
 
-saludar(); */
+function resta(a, b) {
+    let r = a - b
+    alert(r);
+}
 
+function multiplicacion(a, b) {
+    let r = a * b
+    alert(r);
+}
 
+function division(a, b) {
+    let r = a / b
+    alert(r);
+}
+
+suma(1,9)
+ */
+
+// Ejemplo de arrow function o funcion con flecha
 
 const suma  = (a,b) => a + b;
 const resta = (a,b) => a - b;
 
-const impuesto   = x => x * 0.66; 
+const iva   = x => x * 0.66; 
 
-let monto  = 200;
+let monto  = 100;
 let precioDescuento = 0;
 
-let nuevoPrecio = resta(suma(monto, impuesto(monto)), precioDescuento);
+//Calculo el precioProducto + IVA - precioDescueto
+let nuevoPrecio = resta(suma(monto, iva(monto)), precioDescuento);
 console.log(nuevoPrecio);
